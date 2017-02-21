ed65581fc0f3bc9fe3be0ea6483bff21ec99a180
edgracilla <ed.gracilla@gmail.com>
2017-02-21 14:34:50 +0800

    specs changes applied

--- specs/plugins/storage.test.js
+++ specs/plugins/storage.test.js
@@ -15,22 +15,20 @@ const ENV_INPUT_PIPE = 'demo.storage'
 
 describe('Storage Plugin Test', () => {
   before('#test init', () => {
-
     process.env.LOGGERS = ''
     process.env.CONFIG = '{}'
     process.env.EXCEPTION_LOGGERS = ''
     process.env.INPUT_PIPE = ENV_INPUT_PIPE
     process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-    amqp.connect(process.env.BROKER)
-      .then((conn) => {
-        _conn = conn
-        return conn.createChannel()
-      }).then((channel) => {
-        _channel = channel
-      }).catch((err) => {
-        console.log(err)
-      })
+    amqp.connect(process.env.BROKER).then((conn) => {
+      _conn = conn
+      return conn.createChannel()
+    }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
   })
 
   after('terminate connection', () => {
@@ -72,48 +70,40 @@ describe('Storage Plugin Test', () => {
   describe('#logging', () => {
     describe('.log()', () => {
       it('should throw error if logData is empty', (done) => {
-        _plugin.log('')
-          .then(() => {
-            done(new Error('Expecting rejection. Check your test data.'))
-          }).catch((err) => {
-            if (!isEqual(err.message, 'Kindly specify the data to log')) {
-              done(new Error('Returned value not matched.'))
-            } else {
-              done()
-            }
-          })
+        _plugin.log('').then(() => {
+          done(new Error('Expecting rejection. Check your test data.'))
+        }).catch((err) => {
+          if (!isEqual(err.message, 'Please specify a data to log.')) {
+            done(new Error('Returned value not matched.'))
+          } else {
+            done()
+          }
+        })
       })
 
       it('should send a log to logger queues', (done) => {
-        _plugin.log('dummy log data')
-          .then(() => {
-            done()
-          }).catch((err) => {
-            done(err)
-          })
+        _plugin.log('dummy log data').then(() => {
+          done()
+        }).catch(done)
       })
     })
 
     describe('.logException()', () => {
       it('should throw error if param is not an Error instance', (done) => {
-        _plugin.logException('')
-          .then(() => {
-            done(new Error('Expecting rejection. Check your test data.'))
-          }).catch((err) => {
-            if (!isEqual(err.message, 'Kindly specify a valid error to log')) {
-              done(new Error('Returned value not matched.'))
-            } else {
-              done()
-            }
-          })
+        _plugin.logException('').then(() => {
+          done(new Error('Expecting rejection. Check your test data.'))
+        }).catch((err) => {
+          if (!isEqual(err.message, 'Please specify a valid error to log.')) {
+            done(new Error('Returned value not matched.'))
+          } else {
+            done()
+          }
+        })
       })
       it('should send an exception log to exception logger queues', (done) => {
-        _plugin.logException(new Error('test'))
-          .then(() => {
-            done()
-          }).catch((err) => {
-            done(err)
-          })
+        _plugin.logException(new Error('test')).then(() => {
+          done()
+        }).catch(done)
       })
     })
   })
